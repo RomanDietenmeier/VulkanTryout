138baac457ea0c1a4f47c89611dc1d3fecf3cf4f
Roman Dietenmeier <roman@d-robotic.de>
2023-10-27 18:03:07 +0200

    Setup Vulkan in XCode. Create first GLFW window. Ready to instance Vulkan.

--- VulkanTutorialFirstTriangle/main.cpp
+++ VulkanTutorialFirstTriangle/main.cpp
@@ -1,14 +1,54 @@
-//
-//  main.cpp
-//  VulkanTutorialFirstTriangle
-//
-//  Created by Roman Dietenmeier on 27.10.23.
-//
-
+#define GLFW_INCLUDE_VULKAN //#include <vulkan/vulkan.h> is not needed with this and the following line
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+class HelloTriangleApplication {
+    
+public:
+    static const uint32_t WIDTH = 800;
+    static const uint32_t HEIGHT = 600;
+    
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup(); }
+private:
+    GLFWwindow* window;
+    
+    void initWindow(){
+        glfwInit(); //initialize GLFW library
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); //do not create OpenGL context
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); //disable window resizing
+        window = glfwCreateWindow(WIDTH, HEIGHT, "VulkanFirstTriangle", nullptr, nullptr);
+        //width, height, title, specify monitor, smt OpenGL
+    }
+    
+    void initVulkan() {
+    }
+    
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }// wait to receive Window close Event
+    }
+    
+    void cleanup() {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main() {
+    std::cout<<"START main\n";
+    HelloTriangleApplication app;
+    try {
+        app.run();
+    } catch (const std::exception& e) { 
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
