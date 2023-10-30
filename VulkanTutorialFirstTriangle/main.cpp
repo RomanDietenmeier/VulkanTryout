c9426c17c2541a20f15779740d75944dfd1545ee
Roman Dietenmeier <roman@d-robotic.de>
2023-10-30 16:09:37 +0100

    Got my first Vulkan Device to run

--- VulkanTutorialFirstTriangle/main.cpp
+++ VulkanTutorialFirstTriangle/main.cpp
@@ -17,6 +17,7 @@ public:
         cleanup(); }
 private:
     GLFWwindow* window;
+    VkInstance instance;
     
     void initWindow(){
         glfwInit(); //initialize GLFW library
@@ -27,6 +28,7 @@ private:
     }
     
     void initVulkan() {
+        createInstance();
     }
     
     void mainLoop() {
@@ -38,6 +40,43 @@ private:
     void cleanup() {
         glfwDestroyWindow(window);
         glfwTerminate();
+    }
+    
+    void createInstance(){
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        uint32_t glfwExtensionCount = 0; 
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::vector<const char*> requiredExtensions;
+        
+        for(uint32_t i = 0; i < glfwExtensionCount; i++) {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+        //requiredExtensions.emplace_back(VK_KHR_surface);
+        //requiredExtensions.emplace_back(VK_EXT_metal_surface); //does not exists?
+        requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+        requiredExtensions.emplace_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME); //necessary?
+        
+        VkInstanceCreateInfo createInfo{};
+        createInfo.flags|=VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+        //createInfo.flags|=VK_MEMORY_PROPERTY_HOST_COHERENT_BIT; //useless?
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+        createInfo.enabledLayerCount = 0;
+        
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if(result!=VK_SUCCESS){
+            std::cout<<result<<std::endl; //-9 -> Incompatible Driver Error
+            throw std::runtime_error("Failed to create Vulkan instance!");
+        }
     }
 };
 
