f5bd49840d46d62e92891e75715f84a2c8b8dde4
Roman Dietenmeier <roman@d-robotic.de>
2024-02-14 18:53:04 +0100

    Create graphics pipeline but have typos probably does not run!!!

--- VulkanTutorialFirstTriangle/main.cpp
+++ VulkanTutorialFirstTriangle/main.cpp
@@ -104,6 +104,7 @@ private:
     
     VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout;
+    VkPipeline graphicsPipeline;
 
     
     void initWindow(){
@@ -683,6 +684,26 @@ private:
             throw std::runtime_error("Failed to create pipeline layout!");
         }
 
+        VkGraphicsPipelineCreateInfo pipelineInfo{};
+        pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        pipelineInfo.stageCount = 2;
+        pipelineInfo.pStages = shaderStages;
+        pipelineInfo.pVertexInputState = &vertexInputInfo;
+        pipelineInfo.pInputAssemblyState = &inputAssembly;
+        pipelineInfo.pViewportState = &viewportState;
+        pipelineInfo.pRasterizationState = &rasterizer;
+        pipelineInfo.pMultisampleState = &multisampling;
+        pipelineInfo.pDepthStencilState = nullptr;
+        pipelineInfo.pColorBlendState = &colorBlending;
+        pipelineInfo.pDynamicState = &dynamicState;
+        pipelineInfo.layout = pipelineLayout;
+        pipelineInfo.renderPass = renderPass;
+        pipelineInfo.subpass = 0; // index of subpass we want to use
+
+        if(vkCreateGraphicsPipelines(device,VK_NULL_HANDLE,1,&pipelineInfo,nullptr,&graphicsPipeline)!=VK_SUCCESS){
+            throw std::runtime_error("Failed to create graphics pipeline!");
+        }
+
         // The shader modules can be destroyed after the creation of the GraphicsPipeline:
         vkDestroyShaderModule(device,vertShaderModule,nullptr); 
         vkDestroyShaderModule(device,fragShaderModule,nullptr); 
@@ -710,6 +731,7 @@ private:
     }
 
     void cleanup() {
+        vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyRenderPass(device, renderPass, nullptr);
         for (auto imageView : swapChainImageViews) {
